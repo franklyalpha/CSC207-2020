4c2122a37dd4999ce673bc47bc3cc797c493d0f7
Michael Leung <michael.kb.leung@gmail.com>
2020-10-29 18:44:55 -0400

    Modified ID attribute to use UUID. ID will now be of type UUID rather than int.

--- StudyGroups/Chatroom.java
+++ StudyGroups/Chatroom.java
@@ -1,22 +1,20 @@
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class Chatroom {
 
-    private static int count = 0;  // used to update and assign room IDs
-
-    private int id;  // unique identifier for each Chatroom
+    private UUID id;  // unique identifier for each Chatroom
     private ArrayList<String> usersInvolved;  // holds username of all Users in this Chatroom
     private ArrayList<String> historicalChats;  // holds previously sent messages from this Chatroom
 
     public Chatroom(ArrayList<String> users){
-        id = count;  // assign id
-        count += 1;   // update static variable so next Chatroom to be created has different ID
+        id = UUID.randomUUID();  // assign id
         usersInvolved = users;  // specify users in this Chatroom with given parameter
         historicalChats = new ArrayList<String>();  // start with empty chatHistory
     }
 
     // getter for id
-    public int getId(){
+    public UUID getId(){
         return this.id;
     }
 
@@ -30,7 +28,8 @@ public class Chatroom {
         return this.historicalChats;
     }
 
-    // setters TBD
+    // no setters: we don't want to be able to modify the Chatroom ID.
+    // We will 'get' the lists in the UseCase modify the ArrayLists there.
 
 }
 
