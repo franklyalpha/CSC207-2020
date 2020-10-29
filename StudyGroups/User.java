cf1c5087b36694657d35043856ce56e54fcaa0bc
Jingyu Hu <jingyu.hu@mail.utoronto.ca>
2020-10-29 13:33:59 -0400

    m

--- StudyGroups/User.java
+++ StudyGroups/User.java
@@ -1,7 +1,26 @@
 import java.util.ArrayList;
 
 public class User {
+    private String username;
+    private String password;
+    private String userType;
+    private ArrayList<Integer> chatroom;
+    private ArrayList<String> activities;
 
+    public User(String name, String password, String type) {
+        username = name;
+        this.password = password;
+        userType = type;
+        chatroom = new ArrayList<Integer>();
+        activities = new ArrayList<String>();
     }
 
+    public String getUsername() {
+        return username;
+    }
 
+    public boolean setUsername(String newname) {
+        username = newname;
+        return true;
+    }
+}
