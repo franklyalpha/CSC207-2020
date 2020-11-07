7fe829ae6ad429ff4b0431b7a4e9eb325c59ea45
Jingyu Hu <jingyu.hu@mail.utoronto.ca>
2020-11-07 12:10:31 -0500

    deleted the statement to test git

--- phase1/ChatroomManager.java
+++ phase1/ChatroomManager.java
@@ -23,7 +23,6 @@ public class ChatroomManager {
 
     private ArrayList<Chatroom> conferenceChats;
     private ArrayList<Chatroom> privateChats;
-    private int test;
 
 
     public ChatroomManager(ArrayList<String> organizerSpeakers) {
