3d5df41a76daee1f11264393653d7e4b1cfdce53
Jingyu Hu <jingyu.hu@mail.utoronto.ca>
2020-11-18 18:28:40 -0500

    Readme file written.

--- phase1/main/mainEntrance.java
+++ phase1/main/mainEntrance.java
@@ -2,7 +2,7 @@ package main;
 
 import controllers.LoginController;
 
-public class main {
+public class mainEntrance {
     public static void main(String[] args){
         LoginController login = new LoginController();
         login.run();
