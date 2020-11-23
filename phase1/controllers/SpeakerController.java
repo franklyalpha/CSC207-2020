e19bfcf3a5152471aa6dd2fe43d1d076e7ac759a
Jingyu Hu <jingyu.hu@mail.utoronto.ca>
2020-11-23 14:02:04 -0500

    added try-catch block for speaker controller's method

--- phase1/controllers/SpeakerController.java
+++ phase1/controllers/SpeakerController.java
@@ -142,15 +142,34 @@ public class SpeakerController extends OrganizerController {
     }
 
     private UUID findRightChat(ArrayList<String[]> info){
+        int actID;
+        actID = chatIDInput(info);
+        return activityManager.getConferenceChat(
+                UUID.fromString(info.get(actID - 1)[0]));
+    }
+
+    private int chatIDInput(ArrayList<String[]> info) {
+        int actID;
+        while(true){
+            try{
+                actID = determineChatIDValidity(info);
+                break;
+            }catch(IndexOutOfBoundsException e){
+                Presenter.printInvalid("index of chat list");
+            }
+        }
+        return actID;
+    }
+
+    private int determineChatIDValidity(ArrayList<String[]> info)
+            throws IndexOutOfBoundsException{
         Scanner actIDScanner = new Scanner(System.in);
         Presenter.printActivityMessagePrompt();
         int actID = actIDScanner.nextInt();
         if (actID < 1 || actID > info.size()){
-            Presenter.printInvalid("input");
+            throw new IndexOutOfBoundsException("invalid index for chat");
         }
-        UUID chatID = activityManager.getConferenceChat(
-                UUID.fromString(info.get(actID - 1)[0]));
-        return chatID;
+        return actID;
     }
 
 
