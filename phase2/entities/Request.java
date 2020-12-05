d28817eba8360e484ac88d05f6301e60c2d6c74c
Michael Leung <michael.kb.leung@gmail.com>
2020-12-05 18:43:18 -0500

    Creation of new Request entity

---
+++ phase2/entities/Request.java
@@ -0,0 +1,22 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+/**
+ * Represents a user's request to
+ * @author Group 0168
+ * @author CSC207 - Fall 2020
+ * @version 1.0
+ * @since 2.0
+ */
+
+public class Request{
+
+    /**
+     * Unique identifier for each <code>Chatroom</code>.
+     */
+    private final UUID id;  // unique identifier for each entities.Chatroom
+
+
+}
