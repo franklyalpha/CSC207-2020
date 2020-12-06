527a1c89426c279fc456c75e3bd1f5410fdb410f
Michael Leung <michael.kb.leung@gmail.com>
2020-12-06 12:26:07 -0500

    Added ModifyRequestController

--- phase2/Controllers/RequestController.java
+++ phase2/Controllers/RequestController.java
@@ -18,35 +18,6 @@ public class RequestController extends AbstractController {
 
 
     /**
-     * Changes the details of the specified <code>Request</code> to the new specified description.
-     * @param request UUID of the <code>Request</code> for which the description is to be modified.
-     * @param newDetails String representing the new description.
-     */
-    public void modifyRequestDetails(UUID request, String newDetails) throws RequestNotFoundException {
-        if (requestManager.isExistingRequest(request)) {
-            requestManager.modifyDetails(request, newDetails);
-        }
-        else{
-            throw new RequestNotFoundException("Request not found.");
-        }
-    }
-
-    /**
-     * Changes the subject of the specified <code>Request</code> to the new specified subject.
-     * @param request UUID of the <code>Request</code> for which the subject is to be modified.
-     * @param newSubject String representing the new subject.
-     */
-    public void modifyRequestSubject(UUID request, String newSubject) throws RequestNotFoundException {
-        if (requestManager.isExistingRequest(request)) {
-            requestManager.modifySubject(request, newSubject);
-        }
-        else{
-            throw new RequestNotFoundException("Request not found.");
-        }
-    }
-
-
-    /**
      * Removes the specified <code>Request</code> from the system.
      * @param request UUID of the <code>Request</code> that is to be removed.
      */
