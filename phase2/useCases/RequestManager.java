2413d958bf8b3db72226963aae52ff8db5eea605
Michael Leung <michael.kb.leung@gmail.com>
2020-12-05 22:39:13 -0500

    Add methods to RequestManager

--- phase2/useCases/RequestManager.java
+++ phase2/useCases/RequestManager.java
@@ -1,5 +1,6 @@
 package useCases;
 
+import entities.MessageRoom;
 import entities.Request;
 
 import java.util.ArrayList;
@@ -27,11 +28,17 @@ public class RequestManager implements java.io.Serializable{
     private ArrayList<Request> doneRequests;
 
     /**
-     * Creates <code>RequestManager</code> with a blank list of pending requests and a blank list of handled requests.
+     * ArrayList of all instances of <code>Request</code>s.
+     */
+    private ArrayList<Request> requestList;
+
+    /**
+     * Creates <code>RequestManager</code> with a blank list of pending requests, a blank list of handled requests, and a list of all requests.
      */
     public RequestManager(){
         pendingRequests = new ArrayList<>();
         doneRequests = new ArrayList<>();
+        requestList = new ArrayList<>();
     }
 
     /**
@@ -49,5 +56,51 @@ public class RequestManager implements java.io.Serializable{
         return newRequest.getId();
     }
 
+    /**
+     * Handles the specified <code>Request</code>, setting it's status to <code>true</code>, moving it out of the list
+     * of pending requests and into the list of handled requests.
+     * @param requested UUID of the <code>Request</code> to be handled.
+     */
+    public void handleRequest(UUID requested){
+        ArrayList<Request> tmp = new ArrayList<>(pendingRequests);
+        int i = 0;  // index counter
+        for (Request request1 : tmp){
+            if (request1.getId().equals(requested)){           // check the UUID to make sure we have the right entities.Request
+                request1.setStatus(true);
+                this.doneRequests.add(request1);
+                this.pendingRequests.remove(i);
+                }
+           i = i + 1;
+            }
+        }
+
+    /**
+     * Gets the username of the sender of the specified <code>Request</code>.
+     * @param requested UUID of the <code>Request</code> to be handled.
+     * @return String representing the username of the user who sent this <code>Request</code>.
+     */
+    public String getSender(UUID requested){
+        String result = "";
+        ArrayList<Request> tmp = new ArrayList<>(pendingRequests);
+        ArrayList<Request> tmp1 = new ArrayList<>(doneRequests);
+        tmp.addAll(tmp1);  // add the two lists together
+        for (Request request1 : tmp){
+            if (request1.getId().equals(requested)){           // check the UUID to make sure we have the right entities.Request
+               result = request1.getSender();
+               break;
+            }
+        }
+        return result;
+    }
+
+    public void modifyDetails(UUID request, String newDetails){
+        ArrayList<Request> tmp = new ArrayList<>(pendingRequests);
+        ArrayList<Request> tmp1 = new ArrayList<>(doneRequests);
+        tmp.addAll(tmp1);  // add the two lists together
+
+    }
+
 }
+
+
 
