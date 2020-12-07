3777c8c34c50cf506396e8700359e1b7babe4cfb
Michael Leung <michael.kb.leung@gmail.com>
2020-12-06 22:26:27 -0500

    Unified RequestController structure into single file

--- phase2/Controllers/RequestController.java
+++ phase2/Controllers/RequestController.java
@@ -15,5 +15,58 @@ public class RequestController extends AbstractController {
         super(userController);
     }
 
+    /**
+     * Creates a new <code>Request</code> with the current user and the user specified by <code>userName</code>
+     * as the participants.
+     * @param subject String representing a short concise description of the nature of this <code>Request</code>.
+     * @param description String representing a more detailed description of this <code>Request</code>.
+     * @return the UUID of the newly created <code>Request</code>.
+     */
+    public UUID newRequestCreator(String subject, String description){
+        String username = userManager.getUsername();
+        return requestManager.createRequest(username, subject, description);
+    }
+
+    /**
+     * Removes the specified <code>Request</code> from the system.
+     * @param request UUID of the <code>Request</code> that is to be removed.
+     */
+    public void deleteRequest(UUID request) throws RequestNotFoundException {
+        if (requestManager.isExistingRequest(request)) {
+            requestManager.removeRequest(request);
+        }
+        else{
+            throw new RequestNotFoundException("Request not found.");
+        }
+    }
+
+    /**
+     * Changes the details of the specified <code>Request</code> to the new specified description.
+     * @param request UUID of the <code>Request</code> for which the description is to be modified.
+     * @param newDetails String representing the new description.
+     */
+    public void modifyRequestDetails(UUID request, String newDetails) throws RequestNotFoundException {
+        if (requestManager.isExistingRequest(request)) {
+            requestManager.modifyDetails(request, newDetails);
+        }
+        else{
+            throw new RequestNotFoundException("Request not found.");
+        }
+    }
+
+    /**
+     * Changes the subject of the specified <code>Request</code> to the new specified subject.
+     * @param request UUID of the <code>Request</code> for which the subject is to be modified.
+     * @param newSubject String representing the new subject.
+     */
+    public void modifyRequestSubject(UUID request, String newSubject) throws RequestNotFoundException {
+        if (requestManager.isExistingRequest(request)) {
+            requestManager.modifySubject(request, newSubject);
+        }
+        else{
+            throw new RequestNotFoundException("Request not found.");
+        }
+    }
+
 }
 
--- phase2/UI/CreateRequestUI.java
+++ phase2/UI/CreateRequestUI.java
@@ -1,6 +1,6 @@
 package UI;
 
-import Controllers.CreateRequestController;
+import Controllers.RequestController;
 import Controllers.UserController;
 
 import java.util.Scanner;
@@ -9,7 +9,7 @@ public class CreateRequestUI extends AbstractUI {
     /**
      * Instance of <code>CreateRequestController</code>
      */
-    private final CreateRequestController createRequestController;
+    private final RequestController requestController;
 
     /**
      * Creates an instance of <code>CreateRequestUI</code>.
@@ -17,22 +17,22 @@ public class CreateRequestUI extends AbstractUI {
      */
     public CreateRequestUI(UserController userController) {
         super(userController);
-        createRequestController = new CreateRequestController(userController);
+        requestController = new RequestController(userController);
     }
 
     @Override
     public void run() {
-                inputRequestInfo(createRequestController);
+                inputRequestInfo(requestController);
     }
 
-    private void inputRequestInfo(CreateRequestController createRequestController) {
+    private void inputRequestInfo(RequestController requestController) {
         Scanner subjectScanner = new Scanner(System.in);
         System.out.println(userPresenter.strRequestSubjectPrompt());
         String subject = subjectScanner.nextLine();
         Scanner detailScan = new Scanner(System.in);
         System.out.println(userPresenter.strRequestDetailsPrompt());
         String detail = detailScan.nextLine();
-        createRequestController.newRequestCreator(subject, detail);
+        requestController.newRequestCreator(subject, detail);
     }
 
 }
