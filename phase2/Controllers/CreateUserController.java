c2cfa780077f2db0d6d152ff20de95738592d5ea
ally.likehui <kehui.li@mail.utoronto.ca>
2020-12-07 11:21:36 -0500

    change method that create different type of user.

--- phase2/Controllers/CreateUserController.java
+++ phase2/Controllers/CreateUserController.java
@@ -2,45 +2,20 @@ package Controllers;
 
 import globallyAccessible.SpeakerAlreadyExistException;
 import globallyAccessible.UserType;
+import useCases.UserFactory;
 
 public class CreateUserController extends AbstractController{
-    private ICreateUser createAttendee;
-    private ICreateUser createSpeaker;
-    private ICreateUser createOrganizer;
-    private ICreateUser createAdministrator;
     public CreateUserController(UserController userController) {
         super(userController);
-        createSpeaker  = new CreateSpeakerController(userController);
-        createAttendee = new CreateAttendeeController(userController);
-        createOrganizer = new CreateOrganizerController(userController);
-        createAdministrator = new CreateAdministratorController(userController);
     }
     public void ValidateName(String name) throws SpeakerAlreadyExistException {
         if (userManager.isUser(name) !=0){
-            throw new SpeakerAlreadyExistException("Attendee already exist!");
+            throw new SpeakerAlreadyExistException("User already exist!");
         }
     }
 
     public String createUser(UserType type,String name, String password){
-        String userName = null;
-        switch (type) {
-            case ATTENDEE: {
-                userName = createAttendee.createUser(name, password);
-                break;
-            }
-            case SPEAKER: {
-                userName = createSpeaker.createUser(name, password);
-                break;
-            }
-            case ORGANIZER: {
-                userName = createOrganizer.createUser(name, password);
-                break;
-            }
-            case ADMINISTRATOR:{
-                userName = createAdministrator.createUser(name, password);
-                break;
-            }
-        }
-        return userName;
+        new UserFactory(userManager).construct(name, password, type);
+        return name;
     }
 }
