9503cb3c19da4ad8df44cb1ba606eff9ac96e254
Wei Yu <weisandy.yu@mail.utoronto.ca>
2020-12-07 22:24:14 -0500

    fix some errors

--- phase2/UI/OrganizerRemoveEventUI.java
+++ phase2/UI/OrganizerRemoveEventUI.java
@@ -4,17 +4,14 @@ import Controllers.RemoveEventController;
 import Controllers.UserController;
 import globallyAccessible.NoEventsException;
 import menuPresenter.RemoveEventPresenter;
+
 import java.util.Scanner;
 import java.util.UUID;
 
 package useCases;
 
-import entities.Event;
-import entities.Talk;
-import globallyAccessible.EventType;
-
 public class OrganizerModifyEventUI extends AbstractUI {
-    private RemoveEventController removeEvent;
+    private final RemoveEventController removeEvent;
 
     public OrganizerModifyEventUI(UserController userController) {
         super(userController);
@@ -25,55 +22,18 @@ public class OrganizerModifyEventUI extends AbstractUI {
     public void run() {
         RemoveEventPresenter presenter = new RemoveEventPresenter();
         try{
-            inputAndUpdateModification(removeEvent, presenter);
+            inputAndUpdateModification(presenter);
         }catch (NoEventsException e){
             presenter.printNoEvent();
         }
     }
 
-    private void inputAndUpdateModification(RemoveEventController modifyActivity, RemoveEventPresenter presenter)
+    private void inputAndUpdateModification(RemoveEventPresenter presenter)
             throws NoEventsException {
         presenter.printIDForTheEventToBeCancelled(removeEvent.getAllActivities());
         Scanner input_1 = new Scanner(System.in);
-        UUID activityId = UUID.fromString(input_1.nextLine());
+        UUID.fromString(input_1.nextLine());
         removeEvent.cancelEvent();
     }
-}
-
-public class TalkManager extends EventManager {
-    /*
-    Additional Variables:
-    None
-
-    Additional Methods:
-    Speaker manipulators
-    Finder
-     */
-
-    public TalkManager(EventManager eventManager){
-        super(eventManager);
-    }
-
-    public void setSpeaker(UUID actID, String speakerName){
-        Talk targetAct = findEvent(actID);
-        targetAct.setSpeaker(speakerName);
-    }
-
-    public void deleteSpeaker(UUID actID){
-        Talk targetAct = findEvent(actID);
-        targetAct.setSpeaker(null);
-    }
-
-    @Override
-    protected Talk findEvent(UUID actID) {
-        Talk targetAct =  null;
-        for(Event i: this.upcomingEvents.get(EventType.TALK)){
-            if(i.getIdentity().equals(actID)){
-                targetAct = (Talk) i;
-            }
-        }
-        return targetAct;
-    }
-
 }
 
